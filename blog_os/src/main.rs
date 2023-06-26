c2319162fb34d2d2706923ae5b61dfa09e46f8e2
kid <1050109853@qq.com>
2023-06-26 22:34:31 +0800

    finish tests

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -1,24 +1,32 @@
 #![no_std]
 #![no_main]
-mod vga_buffer;
-use core::{panic::PanicInfo};
-// static HELLO:&[u8] = b"Hello World";
-#[panic_handler]
-fn panic(info:&PanicInfo)->!{
-    println!("{}",info);
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use blog_os::println;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
-#[no_mangle]
-pub extern "C" fn _start()-> !{
-    // vga_buffer::print_something();
-    // vga_buffer::WRITER.lock().write_str("hello again").unwrap();
-    // write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
-    // println!("Hello World{}","!");
-    panic!("Some panic message");
-    // loop {}
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
 }
 
-
-
-
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info)
+}
